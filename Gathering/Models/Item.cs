f5ce3515827366214b92184731aeab2f9e69ba07
Michael Bond <codemonkey8510@icloud.com>
2022-06-28 23:00:02 -0400

    Add simple representative JSON data

---
+++ Gathering/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace Gathering.Models;
+
+public class ItemRootobject
+{
+    public Item[] items { get; set; }
+}
+
+public class Item
+{
+    public string id { get; set; }
+    public int slot { get; set; }
+    public string name { get; set; }
+    public string nameFr { get; set; }
+    public string nameDe { get; set; }
+    public string level { get; set; }
+    public string gatheringSkill { get; set; }
+    public string perception { get; set; }
+    public string imageUrl { get; set; }
+    public string description { get; set; }
+    public string descriptionFr { get; set; }
+    public string descriptionDe { get; set; }
+}
---
+++ Gathering/Models/Node.cs
@@ -0,0 +1,29 @@
+﻿namespace Gathering.Models;
+
+public class Rootobject
+{
+    public Node[] nodes { get; set; }
+}
+
+public class Node
+{
+    public string id { get; set; }
+    public string type { get; set; }
+    public string subType { get; set; }
+    public string zone { get; set; }
+    public string zoneFr { get; set; }
+    public string zoneDe { get; set; }
+    public string teleport { get; set; }
+    public string teleportFr { get; set; }
+    public string teleportDe { get; set; }
+    public Position position { get; set; }
+    public string startTime { get; set; }
+    public string endTime { get; set; }
+    public string[] itemIds { get; set; }
+}
+
+public class Position
+{
+    public int x { get; set; }
+    public int y { get; set; }
+}
